beb32fa8382e59241bb36d49f55e0b12bf2dbef4
Carlos-Landerdahl <carloslanderdahl27@gmail.com>
2023-08-03 21:14:04 -0300

    feat: adding text into README.md

---
+++ src/main/java/com/d2devsuperior/eventos/EventosApplication.java
@@ -0,0 +1,13 @@
+package com.d2devsuperior.eventos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventosApplication.class, args);
+	}
+
+}
